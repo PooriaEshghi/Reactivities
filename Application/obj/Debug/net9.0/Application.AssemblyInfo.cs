d80470c7546007ba6d244c264b92fcb270230767
PooriaEshghi <pooriaeshghi@gmail.com>
2024-10-25 16:51:04 +0200

    Create folder structure

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+633379cdc821686e147f91f47180d382d344608e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1493f5b95284bf4cb650c2eea5ebb014e53c8b08")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
