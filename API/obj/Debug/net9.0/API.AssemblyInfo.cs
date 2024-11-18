14572fbaf3716ea14fc3d834ef073e1b560cc3ee
PooriaEshghi <pooriaeshghi@gmail.com>
2024-11-18 18:09:23 +0100

    Adding activiyDashboard and activityDetails

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c31f4aa4953d63c0d05ee3ed2e4b834a18ee0b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9499d16fcca68cc2bcd4e35d7085fbfee6a7585f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,24 +1,26 @@
 import { Grid, GridColumn, List } from 'semantic-ui-react'
 import { Activity } from '../../../app/models/activity'
+import ActivityList from './ActivityList';
+import ActivityDetails from '../detailes/ActivityDetails';
 
 interface Props {
     activities: Activity[];
 }
 
-function ActivityDashboard({activities}:Props) {
-  return (
-    <Grid>
-        <GridColumn width={10}>
-        <List>
-          {activities.map((activity) => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-        </List>
-        </GridColumn>
-    </Grid>
-  )
+function ActivityDashboard({ activities }: Props) {
+    return (
+        <Grid>
+            <GridColumn width={10}>
+                <ActivityList activities={activities} />
+            </GridColumn>
+            <GridColumn width='6'>
+                {activities[0] &&
+
+                    <ActivityDetails activity={activities[0]} />
+                }
+            </GridColumn>
+        </Grid>
+    )
 }
 
 export default ActivityDashboard
